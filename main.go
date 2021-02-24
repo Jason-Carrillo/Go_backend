83cf589674ff76b6917261b9c0a7ef0340490567
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-24 00:06:25 -0700

    added struct of employee

--- main.go
+++ main.go
@@ -1,82 +1,13 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("Drivers:", sql.Drivers())
-
-	//CHOOSE DATABASE TO USE
-	db, err := sql.Open("mysql", "root:codeup@tcp(127.0.0.1:3306)/go_chal_db")
-
-	if err != nil {
-		fmt.Println(err)
+	type employee struct {
+		id         int
+		first_name string
 	}
-	defer db.Close()
-
-	//DROP TABLES
-	dr, err := db.Query("DROP TABLE IF EXISTS product")
-
-	if err != nil {
-		fmt.Println("DROP TABLE ERROR:", err.Error())
-	}
-
-	defer dr.Close()
-
-	//CREATE TABLES
-	// query := `CREATE TABLE IF NOT EXISTS product(product_id int primary key auto_increment, product_name text)`
-
-	crt, err := db.Query("CREATE TABLE IF NOT EXISTS employee(employee_id int primary key auto_increment, first_name VARCHAR(255))")
-
-	if err != nil {
-		fmt.Println("CREATE TABLE ERROR:", err.Error())
-	}
-
-	defer crt.Close()
-
-	//INSERT USER
-	// inst, err := db.Query("INSERT INTO employee(first_name) VALUES ('Caleb')")
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-
-	// defer inst.Close()
-
-	//DELETE USER
-	// inst, err := db.Query("DELETE FROM employee where id = '3'")
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-
-	// defer inst.Close()
-
-	//SELECT FROM A TABLE
-	// sel, err := db.Query("SELECT * FROM employee")
-
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
-
-	// for sel.Next() {
-	// 	var (
-	// 		id   int
-	// 		name string
-	// 	)
-
-	// 	err = sel.Scan(&id, &name)
-	// 	if err != nil {
-	// 		fmt.Println("No users found")
-	// 	}
-
-	// 	fmt.Println(id, name)
-	// }
-
-	// defer sel.Close()
 
 }
--- tables.go
+++ tables.go
@@ -10,29 +10,72 @@ import (
 func tables() {
 	fmt.Println("Drivers:", sql.Drivers())
 
+	//CHOOSE DATABASE TO USE
 	db, err := sql.Open("mysql", "root:codeup@tcp(127.0.0.1:3306)/go_chal_db")
 
 	if err != nil {
 		fmt.Println(err)
-		db.Close()
+	}
+	defer db.Close()
+
+	//DROP TABLES
+	dr, err := db.Query("DROP TABLE IF EXISTS product")
+
+	if err != nil {
+		fmt.Println("DROP TABLE ERROR:", err.Error())
 	}
 
-	_, err = db.Exec("go_chal_db")
+	defer dr.Close()
+
+	//CREATE TABLES
+
+	crt, err := db.Query("CREATE TABLE IF NOT EXISTS employee(employee_id int primary key auto_increment, first_name VARCHAR(255))")
+
 	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("DB selected successfully..")
+		fmt.Println("CREATE TABLE ERROR:", err.Error())
 	}
 
-	stmt, err := db.Prepare("CREATE Table employee(id int NOT NULL AUTO_INCREMENT, first_name varchar(50), last_name varchar(30), PRIMARY KEY (id));")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Println("Table created successfully..")
-	}
+	defer crt.Close()
+
+	//INSERT USER
+	// inst, err := db.Query("INSERT INTO employee(first_name) VALUES ('Caleb')")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// defer inst.Close()
+
+	//DELETE USER
+	// inst, err := db.Query("DELETE FROM employee where id = '3'")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// defer inst.Close()
+
+	//SELECT FROM A TABLE
+	// sel, err := db.Query("SELECT * FROM employee")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// for sel.Next() {
+	// 	var (
+	// 		id   int
+	// 		name string
+	// 	)
+
+	// 	err = sel.Scan(&id, &name)
+	// 	if err != nil {
+	// 		fmt.Println("No users found")
+	// 	}
+
+	// 	fmt.Println(id, name)
+	// }
+
+	// defer sel.Close()
 
 }
