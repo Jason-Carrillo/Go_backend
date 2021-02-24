ccfadbd5392ddf7527e91a9b5974b62792c9fea1
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-23 23:43:59 -0700

    added correct query string to add table

--- main.go
+++ main.go
@@ -18,14 +18,25 @@ func main() {
 	}
 	defer db.Close()
 
+	//DROP TABLES
+	dr, err := db.Query("DROP TABLE IF EXISTS product")
+
+	if err != nil {
+		fmt.Println("DROP TABLE ERROR:", err.Error())
+	}
+
+	defer dr.Close()
+
 	//CREATE TABLES
-	// crt, err := db.Query("CREATE TABLE employee(id int AUTO INCREMENT, first_name VARCHAR(255))")
+	// query := `CREATE TABLE IF NOT EXISTS product(product_id int primary key auto_increment, product_name text)`
 
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// }
+	crt, err := db.Query("CREATE TABLE IF NOT EXISTS employee(employee_id int primary key auto_increment, first_name VARCHAR(255))")
 
-	// defer crt.Close()
+	if err != nil {
+		fmt.Println("CREATE TABLE ERROR:", err.Error())
+	}
+
+	defer crt.Close()
 
 	//INSERT USER
 	// inst, err := db.Query("INSERT INTO employee(first_name) VALUES ('Caleb')")
@@ -36,36 +47,36 @@ func main() {
 
 	// defer inst.Close()
 
-	//INSERT USER
-	inst, err := db.Query("DELETE FROM employee where id = '3'")
+	//DELETE USER
+	// inst, err := db.Query("DELETE FROM employee where id = '3'")
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 
-	defer inst.Close()
+	// defer inst.Close()
 
 	//SELECT FROM A TABLE
-	sel, err := db.Query("SELECT * FROM employee")
+	// sel, err := db.Query("SELECT * FROM employee")
 
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 
-	for sel.Next() {
-		var (
-			id   int
-			name string
-		)
+	// for sel.Next() {
+	// 	var (
+	// 		id   int
+	// 		name string
+	// 	)
 
-		err = sel.Scan(&id, &name)
-		if err != nil {
-			fmt.Println("No users found")
-		}
+	// 	err = sel.Scan(&id, &name)
+	// 	if err != nil {
+	// 		fmt.Println("No users found")
+	// 	}
 
-		fmt.Println(id, name)
-	}
+	// 	fmt.Println(id, name)
+	// }
 
-	defer sel.Close()
+	// defer sel.Close()
 
 }
